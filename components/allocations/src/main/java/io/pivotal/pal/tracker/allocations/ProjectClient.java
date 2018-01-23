fe8a691f41948e1b2ba23b00bfb7f8024f6373fe
weisu00 <weisu00@gmail.com>
2018-01-23 08:48:36 -0700

    add circuit breaker project client

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,34 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private final Logger logger = LoggerFactory.getLogger(ProjectClient.class);
+    private final Map<Long, ProjectInfo> cache = new ConcurrentHashMap<Long, ProjectInfo>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.put(projectId, projectInfo);
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return cache.get(projectId);
     }
 }
--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,18 +1,36 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String endpoint;
+    private final Logger logger = LoggerFactory.getLogger(ProjectClient.class);
+    private final Map<Long, ProjectInfo> cache = new ConcurrentHashMap<Long, ProjectInfo>();
+
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.put(projectId, projectInfo);
+        return projectInfo;
     }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Getting project with id {} from cache", projectId);
+        return cache.get(projectId);
+    }
+
 }
